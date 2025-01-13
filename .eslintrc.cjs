861d61c20617e031c03e70f3c3f91e6fce8e265e
jose frometa <jfrometa@gmail.com>
2025-01-13 09:19:44 -0400

    updates eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,19 @@ module.exports = {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
-  ignores: [
-    '**/.git',
-    '**/.hg',
-    '**/.pnp.*',
-    '**/.svn',
-    '**/.yarn/**',
-    '**/build',
-    '**/dist/**',
-    '**/node_modules',
-    '**/temp',
-    '**/.tmp',
-    '**/.cache',
-    'playwright.config.ts',
-    'jest.config.js'
-  ]
+  // ignores: [
+  //   '**/.git',
+  //   '**/.hg',
+  //   '**/.pnp.*',
+  //   '**/.svn',
+  //   '**/.yarn/**',
+  //   '**/build',
+  //   '**/dist/**',
+  //   '**/node_modules',
+  //   '**/temp',
+  //   '**/.tmp',
+  //   '**/.cache',
+  //   'playwright.config.ts',
+  //   'jest.config.js'
+  // ]
 }
